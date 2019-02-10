2ffc08cb72539b09e19d9386c465ecd685e22fb0
Eko <swdev.bali@gmail.com>
2019-02-11 01:18:09 +0700

    fix location

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'products')
 
 CART_SESSION_ID = 'cart'
